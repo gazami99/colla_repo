8fcf29a85645ff8f266fde9ba5e76214b64fb562
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-09-07 17:43:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("test")
+def capitalize_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.capitalize()
+
+# 테스트코드
+def test_capitalize_string():
+    assert capitalize_string('test') == 'Test'
