4faa81b3b6f2907e4d6a2647e3a08fe05a35a947
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-09-10 02:10:46 +0900

    Update test_fucntion.py

--- test_fucntion.py
+++ test_fucntion.py
@@ -2,14 +2,14 @@
 from reqJSON import ServerManager
 from real import getCovidinfo,loadInfo
 
-def test_run_stuff():
+def test_run_stuff(): # 접속 잘되는지 테스트 
     url = 'http://apis.data.go.kr/1352000/ODMS_COVID_05/callCovid05Api'
     params ={'serviceKey' : '8a9K+8YsLdMkCpBZbqqksp85xu/d2dmUSaw3tamfh9QDthpu9xkpYsTN4D0VZCUjq7G/jBPzUesPOT3EhzdynQ==', 'pageNo' : '1', 'numOfRows' : '500', 'apiType' : 'json', 'create_dt' : '2022-01-08' } 
     result = ServerManager(url,params)
     
     assert result.urlCheck == True
 
-def test_getcovid():
+def test_getcovid():   #   df가 잘 나오는지 테스트 
     date = '2022-05-01'
     test_df = getCovidinfo(date)
     
