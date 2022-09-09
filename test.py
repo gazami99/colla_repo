2a1b432e01c61e4f397075a72dc0a83de67899a5
Luke-Eom <eluke1028@gmail.com>
2022-09-09 23:45:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -15,3 +15,22 @@ df.columns = ['확진자수','확진율', '등록일자', '치명율', '사망�
 
 df = pd.DataFrame.from_dict(json_body["response"]["body"]["items"]["item"])
 print(df.sort_values(by="gubun"))
+
+# data loading for문 to_sql -> column names 바꾸고 넣었을때 ㄱㅊ을까? DTO와 JSON데이터의 key값의 명칭이 다를경우?
+# to_sql후 다시 JSON으로 뽑아오는 것 ㄱㅊ?
+
+# 비동기
+#  GET or POST
+
+# 1. 수치형 (통계 및 계산적 관점에서 필요)
+# 	- 테이블
+# 2. 시각화 (발표 및 동향 파악에 필요)
+#  - 
+
+# db에 모두 로드된 후 
+
+# 구분별 총 확진자 수 , 확진율(sum/dts)
+# 총 사망자수 사망률(확인 필요)
+# ---> 이부분 코드 짜기
+
+# pyplot 
