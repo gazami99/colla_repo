d8c9f0bf89a15846feaad477c765497b08d007e7
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-09-10 02:05:17 +0900

    Update test_reqJSON.py

--- test_reqJSON.py
+++ test_reqJSON.py
@@ -1,6 +1,6 @@
 #test_reqJSON.py
 from reqJSON import ServerManager
-import real
+from real import getCovidinfo,loadInfo
 
 def test_run_stuff():
     url = 'http://apis.data.go.kr/1352000/ODMS_COVID_05/callCovid05Api'
@@ -8,3 +8,16 @@ def test_run_stuff():
     result = ServerManager(url,params)
     
     assert result.urlCheck == True
+
+def test_getcovid():
+    date = '2022-05-01'
+    test_df = getCovidinfo(date)
+    
+    assert not test_df.any == False
+    
+
+def test_load():
+    
+    result = loadInfo()
+    
+    assert result == True
