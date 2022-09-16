e61dd35fb97521ed856c53ac672ca9e55f5075a6
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-09-16 18:03:35 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,17 +1,1 @@
- USE test;
-
-DROP TABLE IF EXISTS covid_info;
-    
-CREATE TABLE covid_info
-(            
-   info_no int auto_increment,
-   conf_case int,
-   conf_caserate int,
-   create_dt varchar(20),
-   critical_rate int,
-   death int,
-   death_rate int,
-   gubun varchar(20),
-   CONSTRAINT pk_covidinfo PRIMARY KEY ( info_no )
-);
 
